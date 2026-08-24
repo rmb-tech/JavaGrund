a3e62743e7b21d58a118b5bcd3f4113e66170698
rmb-tech <rickard@rmbtech.se>
2026-08-24 13:21:59 +0200

    G1 Hälsning och namn

--- src/main/java/Lektion1/HelloWorld.java
+++ src/main/java/Lektion1/HelloWorld.java
@@ -3,5 +3,7 @@ package Lektion1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hej Världen!");
+        System.out.println("Mitt namn är Rickard");
+        System.out.println("Dagens datum är 2026-08-24");
     }
 }
