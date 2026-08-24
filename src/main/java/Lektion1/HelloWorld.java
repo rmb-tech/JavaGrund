ae1c05cb39b46fcedb024a30f2bc1bdfde40dcfc
rmb-tech <rickard@rmbtech.se>
2026-08-24 13:17:43 +0200

    Skapat strukturen för repo och gjort ett enkelt HelloWorld skript

--- src/main/java/Lektion1/HelloWorld.java
+++ src/main/java/Lektion1/HelloWorld.java
@@ -1,4 +1,8 @@
 package Lektion1;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hej Världen!");
+
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Programmering med Java, grund");
+
+
+        //Notera radbrytning med \n ovan. Nedan bjussar jag på en gratis for-loop
+        for (int i = 1; i <= 5; i++) {
+            int countdownInt = 5 - i;
+            System.out.println(countdownInt + 1);
+            if (countdownInt == 0) {
+                System.out.println("TAKEOFF!");
+            }
+        }
+    }
 }
