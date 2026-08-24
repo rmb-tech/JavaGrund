b72d55ca5d4c33910bb2d154409e2f8b410326cc
rmb-tech <rickard@rmbtech.se>
2026-08-24 13:29:13 +0200

    G3 Kursfakta

---
+++ src/main/java/Lektion1/Kursfakta.java
@@ -0,0 +1,10 @@
+package Lektion1;
+
+public class Kursfakta {
+    public static void main(String[] args) {
+        System.out.println("Kurs: Programmering med Java, grund");
+        System.out.println("Längd: 8 veckor");
+        System.out.println("Antal inlämningar: 2");
+        System.out.println("Inlämning 1 deadline: Fredag 25:e september");
+    }
+}
