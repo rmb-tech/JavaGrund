21d88aca34563c2b86f6a8c6da2a6f1564444027
rmb-tech <rickard@rmbtech.se>
2026-08-24 14:03:45 +0200

    VG5 README och lade till en emoji i HelloWorld.java

---
+++ README.md
@@ -0,0 +1,9 @@
+# Java - Grund
+Innehåller filer som används i utbildningen.
+
+* Lektion 1
+
+## Lektion 1
+### HelloWorld
+
+Detta är ett enkelt första Java-program som skapats som skriver ut "Hej världen!"
--- src/main/java/Lektion1/HelloWorld.java
+++ src/main/java/Lektion1/HelloWorld.java
@@ -3,7 +3,7 @@ package Lektion1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("+----------------+");
-        System.out.println("|  Hej Världen!  |");
+        System.out.println("| Hej Världen 🌍 |");
         System.out.println("+----------------+");
         System.out.println("Mitt namn är Rickard");
         System.out.println("Dagens datum är 2026-08-24");
