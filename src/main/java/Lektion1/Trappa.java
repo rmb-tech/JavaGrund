71d923365a067549809601c97f4c697bc0c2822d
rmb-tech <rickard@rmbtech.se>
2026-08-24 13:42:38 +0200

    VG2 Trappa

---
+++ src/main/java/Lektion1/Trappa.java
@@ -0,0 +1,11 @@
+package Lektion1;
+
+public class Trappa {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+}
