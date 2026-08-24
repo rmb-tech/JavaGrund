07d39d4acbe12aa822f280bf5ff1e346434081e3
rmb-tech <rickard@rmbtech.se>
2026-08-24 13:39:57 +0200

    G7 Adressetikett

---
+++ src/main/java/Lektion1/Adressetikett.java
@@ -0,0 +1,10 @@
+package Lektion1;
+
+public class Adressetikett {
+    public static void main(String[] args) {
+        System.out.println("Anna Svensson");
+        System.out.println("Storgatan 1");
+        System.out.println("411 01 Göteborg");
+        System.out.println("Sverige");
+    }
+}
