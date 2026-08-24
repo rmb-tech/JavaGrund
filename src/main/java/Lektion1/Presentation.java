0e0143dd1b135662a30bcb0972e49ed1eae8a3ae
rmb-tech <rickard@rmbtech.se>
2026-08-24 13:26:05 +0200

    G2 Presentera dig

---
+++ src/main/java/Lektion1/Presentation.java
@@ -0,0 +1,10 @@
+package Lektion1;
+
+public class Presentation  {
+    public static void main(String[] args) {
+        System.out.println("Namn: Rickard Malm");
+        System.out.println("Stad: Dals-Ed");
+        System.out.println("Jag går utbildningen för att jag vill bli anställningsbar som fullstack-utvecklare");
+        System.out.println("Kul fakta: Jag har en svart och en grå katt samt ett par akvarium");
+    }
+}
