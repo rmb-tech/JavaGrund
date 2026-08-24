1f95291aedd90976ec588f54d2fb82301c7b0e5f
rmb-tech <rickard@rmbtech.se>
2026-08-24 13:48:18 +0200

    VG3 Visitkort

---
+++ src/main/java/Lektion1/Visitkort.java
@@ -0,0 +1,11 @@
+package Lektion1;
+
+public class Visitkort {
+    public static void main(String[] args){
+        System.out.println("==========================================");
+        System.out.println("||  Rickard Malm                        ||");
+        System.out.println("||  Java-utvecklare (under utbildning)  ||");
+        System.out.println("||  github.com/rmb-tech                 ||");
+        System.out.println("==========================================");
+    }
+}
