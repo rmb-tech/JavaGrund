3b89a80279e03456191e24b2f25ebc6cd8fc6b00
rmb-tech <rickard@rmbtech.se>
2026-08-24 13:50:42 +0200

    VG4 Kvitto

---
+++ src/main/java/Lektion1/Kvitto.java
@@ -0,0 +1,13 @@
+package Lektion1;
+
+public class Kvitto {
+    public static void main(String[] args){
+        System.out.println("KVITTO - Systementor Kiosk");
+        System.out.println("------------------------------");
+        System.out.println("Kaffe                    25 kr");
+        System.out.println("Smörgås                  45 kr");
+        System.out.println("Äpple                    10 kr");
+        System.out.println("------------------------------");
+        System.out.println("Summa                    80 kr");
+    }
+}
