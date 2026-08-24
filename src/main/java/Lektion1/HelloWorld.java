a8f7b472ba03f517be18ccd795642b6398c71085
rmb-tech <rickard@rmbtech.se>
2026-08-24 13:41:25 +0200

    VG1 Ram runt texten

--- src/main/java/Lektion1/HelloWorld.java
+++ src/main/java/Lektion1/HelloWorld.java
@@ -2,7 +2,9 @@ package Lektion1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hej Världen!");
+        System.out.println("+----------------+");
+        System.out.println("|  Hej Världen!  |");
+        System.out.println("+----------------+");
         System.out.println("Mitt namn är Rickard");
         System.out.println("Dagens datum är 2026-08-24");
     }
