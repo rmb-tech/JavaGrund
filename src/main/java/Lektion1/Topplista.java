90d17221b9fedd0780b320e9fa11dbf9342d715c
rmb-tech <rickard@rmbtech.se>
2026-08-24 13:31:00 +0200

    G4 Topplista

---
+++ src/main/java/Lektion1/Topplista.java
@@ -0,0 +1,10 @@
+package Lektion1;
+
+public class Topplista {
+    public static void main(String[] args) {
+        System.out.println("Mina 3 favoritspel:");
+        System.out.println("1. Path of Exile 1 och 2");
+        System.out.println("2. Dark age of Camelot");
+        System.out.println("NHL till PS5");
+    }
+}
