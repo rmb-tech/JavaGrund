b3fcf8fb26437c9605e0fa20864c85655aad3be8
rmb-tech <rickard@rmbtech.se>
2026-08-26 09:19:35 +0200

    Ändrade datum till dagens datum

--- src/main/java/Lektion1/HelloWorld.java
+++ src/main/java/Lektion1/HelloWorld.java
@@ -2,10 +2,11 @@ package Lektion1;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        String namn = "Rickard Malm";
         System.out.println("+----------------+");
         System.out.println("| Hej Världen 🌍 |");
         System.out.println("+----------------+");
-        System.out.println("Mitt namn är Rickard Malm");
+        System.out.println("Mitt namn är " + namn);
         System.out.println("Dagens datum är 2026-08-26");
     }
 }
