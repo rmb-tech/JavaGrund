51612302e2bae453250acd7e79f0dc63c422c144
rmb-tech <rickard@rmbtech.se>
2026-08-26 09:21:57 +0200

    Lade till vars för namn och dagens datum.

--- src/main/java/Lektion1/HelloWorld.java
+++ src/main/java/Lektion1/HelloWorld.java
@@ -1,12 +1,17 @@
 package Lektion1;
 
+import java.time.LocalDate;
+
 public class HelloWorld {
     public static void main(String[] args) {
+
         String namn = "Rickard Malm";
+        String today = LocalDate.now().toString();
+
         System.out.println("+----------------+");
         System.out.println("| Hej Världen 🌍 |");
         System.out.println("+----------------+");
         System.out.println("Mitt namn är " + namn);
-        System.out.println("Dagens datum är 2026-08-26");
+        System.out.println("Dagens datum är " + today);
     }
 }
